ef5f688f6118c5cc25c768b6bcfb717c54a56ed1
Salazar2210 <rafael.sac@puccampinas.edu.br>
2026-04-23 14:27:56 -0300

    Editar dados do eleitor

--- codigo/funcoesEleitor.py
+++ codigo/funcoesEleitor.py
@@ -94,3 +94,101 @@ def deletar_eleitor(cpf, titulo):
     except Exception as e:
         conexaobd.conexao.rollback()
         print(f'Eleitor não encontrado! {e}')
+
+def alterar_dados_eleitor(nome, numero_titulo, cpf, mesario, chave_acesso):
+    try:
+        cpf = input("Digite o CPF do eleitor: ")
+        sql_busca = "SELECT * FROM eleitores WHERE cpf = %s"
+        conexaobd.cursor.execute(sql, ) # não sei oq colocar dentro do ()
+        eleitor = conexaobd.cursor.fetchone()
+        if eleitor is None:
+            print("\n=====================================")
+            print("❌ ELEITOR NÃO ENCONTRADO!")
+            print("=====================================\n")
+
+        print("\n=====================================")
+        print(f"\nELEITOR ENCONTRADO: {nome}")
+        print("=====================================\n")
+
+        print("\n=====================================")
+        print("  O QUE DESEJA EDITAR?")
+        print("=====================================\n")
+        print("\nO que deseja editar?")
+        print("1 - Nome")
+        print("2 - Número do titulo de eleitor")
+        print("3 - CPF")
+        print("4 - Status de mesário")
+        print("0 - Cancelar")
+        opcao = input("\nEscolha uma opção: ")
+
+    
+        match opcao:
+            case 0:
+                print("Edição cancelada!")
+                input("Pressione Enter para voltar a tela inicial...")
+                return
+            case 1:
+                novo_nome = input("Informe o  novo nome do eleitor: ")
+                sql = "UPDATE eleitores SET nome = %s, WHERE cpf = %s"
+                conexaobd.cursor.execute(sql, ) # nao sei oq coloca dentro do ()
+            case 2:
+                novo_titulo = input("Digite o novo número do título: ")
+                sql = "UPDATE eleitores SET numero_titulo = %s WHERE cpf = %s"
+                conexaobd.cursor.execute(sql, ) #nao sei oq coloca dentro do ()
+            case 3:
+                novo_cpf = input("Digite o novo CPF: ")
+                sql = "UPDATE eleitores SET cpf = %s WHERE cpf = %s"
+                conexaobd.cursor.execute(sql, ) #nao sei oq coloca dentro do ()
+            case 4:
+                print("Eleitor é mesário?")
+                print("1 - Sim")
+                print("2 - Não")
+                opcao_mesario = input("Escolha: ")
+                match opcao_mesario:
+                    case 1:
+                        opcao_mesario == 1
+                        novo_valor = True
+                    case 2:
+                        opcao_mesario == 2
+                        novo_valor = False
+                    case _:
+                        print("\n❌ Opção inválida!")
+                        input("\nPressione Enter para voltar a tela inicial...")
+                        return
+
+                sql = "UPDATE eleitores SET mesario = %s WHERE cpf = %s"
+                conexaobd.cursor.execute(sql, ) #nao sei oq coloca dentro do ()
+            case _:
+                    print("\n❌ Opção inválida!")
+                    input("\nPressione Enter para voltar a tela inicial...")
+                    return
+        conexaobd.conexao.commit(sql, )
+        print("\n=====================================")
+        print("✅ DADOS ATUALIZADOS COM SUCESSO!")
+        print("=====================================\n")
+        print(f"Nome: {nome}")
+        print(f"Título: {numero_titulo}")
+        print(f"CPF: {cpf}")
+        print(f"Chave de acesso: {chave_acesso}")
+        print(f"Mesário: {'Sim' if mesario else 'Não'}")
+        input("\nPressione Enter para voltar a tela inicial...")
+   
+    except mysql.connector.IntegrityError as err:
+        match "cpf" in str(err).lower(), "numero_titulo" in str(err).lower():
+            case (True, _):
+                print("\n❌ Erro: Este CPF já está cadastrado no sistema!")
+                input("\nPressione Enter para voltar a tela inicial...")
+            case (_, True):
+                print("\n❌ Erro: Este título já está cadastrado no sistema!")
+                input("\nPressione Enter para voltar a tela inicial...")
+            case _:
+                print(f"\n❌ Erro: {err}")
+
+    except mysql.connector.Error as err:
+        print(f"\n❌ Erro ao editar no banco de dados: {err}")
+        input("\nPressione Enter para voltar a tela inicial...")
+
+    finally:
+        if 'conn' in locals() and conexaobd.conexao.is_connected():
+            conexaobd.cursor.close()
+            conexaobd.conexao.close()
